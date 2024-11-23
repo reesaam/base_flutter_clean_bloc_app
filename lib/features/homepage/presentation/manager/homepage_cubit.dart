901f80a2c4c4308293945dee79a8283e6e5257b5
Resam Taghipour <resam.t@gmail.com>
2024-11-23 14:52:09 +0330

    HomePage

--- lib/features/homepage/presentation/manager/homepage_cubit.dart
+++ lib/features/homepage/presentation/manager/homepage_cubit.dart
@@ -1,14 +1,26 @@
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 import '../../../../core/annotations/dependency_injection_annotation.dart';
 import '../../../../core/annotations/freezed_custom_annotation.dart';
+import '../../../../core/bloc_core_functions.dart';
+import '../../../../core/core_elements/core_cubit.dart';
+import '../../../../core/core_resources/page_details.dart';
+import '../../../../shared/shared_mems/core_mems/app_page_detail_mem/app_page_detail.dart';
 
 part 'homepage_state.dart';
 part 'homepage_cubit.freezed.dart';
 
 @DI.controller
-class HomepageCubit extends Cubit<HomepageState> {
+class HomepageCubit extends CoreCubit<HomepageState> {
   HomepageCubit() : super(const HomepageState.initial());
+
+  @override
+  AppPageDetailEntity get pageDetail => AppPageDetailsList.homepage;
+
+  @override
+  List<BlocProvider> get cubits => [
+    provideBlocProvider<HomepageCubit>(),
+  ];
 }
